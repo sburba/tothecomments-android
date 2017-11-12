4282133147cb05636fa834ca285690fe2cf44998
Sam Burba <gitlab@samburba.com>
2017-11-12 07:57:51 -0500

    Move Reddit response data classes into RedditHttpService

--- app/src/main/java/io/burba/tothecomments/network/reddit/RedditHttpService.kt
+++ app/src/main/java/io/burba/tothecomments/network/reddit/RedditHttpService.kt
@@ -4,6 +4,15 @@ import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
+internal data class SearchResponse(val data: SearchResponseContent)
+internal data class SearchResponseContent(val children: List<LinkWrapper>)
+internal data class LinkWrapper(val data: Link)
+internal data class Link(
+        val title: String,
+        val permalink: String,
+        val subreddit: String
+)
+
 internal interface RedditHttpService {
     // This is a stupid hack
     @GET("search.json?sort=comments&limit=5")
