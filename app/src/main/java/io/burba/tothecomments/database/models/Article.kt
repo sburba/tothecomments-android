62af0f335305c0134f0842678baea5ce1b9c9050
Sam Burba <gitlab@samburba.com>
2017-11-12 09:24:41 -0500

    Stop using paperparcel because I don't need parcelable right now

--- app/src/main/java/io/burba/tothecomments/database/models/Article.kt
+++ app/src/main/java/io/burba/tothecomments/database/models/Article.kt
@@ -5,10 +5,7 @@ import android.arch.persistence.room.Entity
 import android.arch.persistence.room.Index
 import android.arch.persistence.room.PrimaryKey
 import org.threeten.bp.Instant
-import paperparcel.PaperParcel
-import paperparcel.PaperParcelable
 
-@PaperParcel
 @Entity(tableName = "articles",
         indices = arrayOf(Index(
                 value = "url",
@@ -21,10 +18,4 @@ data class Article(
         val url: String,
         @ColumnInfo(name = "last_search_time")
         val lastSearchTime: Instant = Instant.now()
-) : PaperParcelable {
-    // Parcelable makes me so sad
-    companion object {
-        @JvmField
-        val CREATOR = PaperParcelArticle.CREATOR
-    }
-}
+)
