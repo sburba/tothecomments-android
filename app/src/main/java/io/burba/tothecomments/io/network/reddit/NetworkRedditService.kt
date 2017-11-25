a90c8aac38c38379b6531fa47cb18b31961c0d73
Sam Burba <gitlab@samburba.com>
2017-11-25 13:08:48 -0500

    Specify User-Agent in header in reddit api requests

--- app/src/main/java/io/burba/tothecomments/io/network/reddit/NetworkRedditService.kt
+++ app/src/main/java/io/burba/tothecomments/io/network/reddit/NetworkRedditService.kt
@@ -1,13 +1,27 @@
 package io.burba.tothecomments.io.network.reddit
 
+import io.burba.tothecomments.BuildConfig
 import io.reactivex.Single
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
+private const val USER_AGENT = "android:io.burba.tothecomments:v${BuildConfig.VERSION_NAME} (by /u/thisnameistoolon)"
+
 object NetworkRedditService {
+    private val httpClient = OkHttpClient().newBuilder().addInterceptor({ chain ->
+        chain.proceed(
+                chain.request()
+                        .newBuilder()
+                        .addHeader("User-Agent", USER_AGENT)
+                        .build()
+        )
+    }).build()
+
     private val retrofit = Retrofit.Builder()
             .baseUrl("https://www.reddit.com/")
+            .client(httpClient)
             .addConverterFactory(MoshiConverterFactory.create())
             .addCallAdapterFactory(RxJava2CallAdapterFactory.createAsync())
             .build()
